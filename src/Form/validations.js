538c315624ce3dc53040eeda41d756bf07684089
vgamez <v.gamez@englobatech.com>
2021-07-06 10:39:23 +0200

    validation

--- src/Form/validations.js
+++ src/Form/validations.js
@@ -1,3 +1,46 @@
+const NIF_LETTERS = 'TRWAGMYFPDXBNJZSQVHLCKE';
+const NIF_MAX_LENGTH = 9;
+const NIF_MIN_LENGTH = 8;
+const NIE_LETTERS = 'XYZ';
+const NIF_REGEX = /^(\d{8})([A-Z])$/;
+const NIE_REGEX = /^[XYZ]\d{7,8}[A-Z]$/;
+
+function validateNIF(documentId) {
+	let isValid = false;
+
+	if (!documentId) {
+		return isValid;
+	}
+
+	const hasCorrectLength =
+		documentId.length >= NIF_MIN_LENGTH && documentId.length <= NIF_MAX_LENGTH;
+
+	if (hasCorrectLength) {
+		const NIFLetter = documentId[documentId.length - 1].toUpperCase();
+		const NIFNumber =
+			parseInt(documentId.substring(0, documentId.length - 1)) % 23;
+
+		isValid = NIFLetter === NIF_LETTERS[NIFNumber];
+	}
+
+	return isValid;
+}
+
+function validateNIE(documentId) {
+	if (!documentId) {
+		return false;
+	}
+
+	const firstLetter = documentId[0].toUpperCase();
+	const prefix = NIE_LETTERS.indexOf(firstLetter);
+
+	if (!prefix) {
+		return false;
+	}
+
+	return validateNIF(prefix + documentId.substring(1));
+}
+
 export const validations = {
 	hourFormat: {
 		name: 'hourFormat',
@@ -9,50 +52,11 @@ export const validations = {
 	},
 	nifFormat: {
 		name: 'nifFormat',
-		cb: (value) => value.match(/^(d{8})([A-Z])$/) && validateNIF(value),
+		cb: (value) => value.match(NIF_REGEX) && validateNIF(value),
 	},
 	nieFormat: {
 		name: 'nieFormat',
-		cb: (value) => value.match(/^[XYZ]d{7,8}[A-Z]$/) && validateNIE(value),
+		cb: (value) => value.match(NIE_REGEX) && validateNIE(value),
 	},
 	// more validations here
 };
-
-function validateNIF(documentId) {
-	const letrasNIF = 'TRWAGMYFPDXBNJZSQVHLCKE';
-	const NIF_maxlength = 9;
-	const NIF_minlength = 8;
-
-	if (!documentId) {
-		return false;
-	}
-
-	const hasCorrectLength =
-		documentId.length >= NIF_minlength && documentId.length <= NIF_maxlength;
-
-	if (hasCorrectLength) {
-		let letraNIF = documentId[documentId.length - 1].toUpperCase();
-		let numeroNIF =
-			parseInt(documentId.substring(0, documentId.length - 1)) % 23;
-
-		return letraNIF === letrasNIF[numeroNIF] ? true : false;
-	}
-
-	return false;
-}
-
-function validateNIE(documentId) {
-	if (!documentId) {
-		return false;
-	}
-
-	const letrasControl = 'XYZ';
-	const letraInicio = documentId[0].toUpperCase();
-	const prefijo = letrasControl.indexOf(letraInicio);
-
-	if (prefijo === undefined || prefijo === null) {
-		return false;
-	}
-
-	return validateNIF(prefijo + documentId.substring(1));
-}
